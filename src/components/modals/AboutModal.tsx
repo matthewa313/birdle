6eedff29a8f570b994dd2bf4b5e73219c8a1ec4b
Matthew Anderson <mwanderson04@gmail.com>
2022-02-12 10:38:35 -0700

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,7 +9,7 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Creek Birdle. Wordle, about birds only. Concept by Steele Billings. Code by Matthew Anderson.
+        Creek's Birdle. Wordle, about birds only. Concept by Steele Billings. Code by Matthew Anderson.
       </p>
     </BaseModal>
   )
