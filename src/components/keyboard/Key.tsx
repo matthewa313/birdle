ba67501699143249c09364e18830aad3a2d794a1
Matthew Anderson <mwanderson04@gmail.com>
2022-02-13 21:42:29 -0700

    Update Key.tsx

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react'
 import classnames from 'classnames'
 import { CharStatus } from '../../lib/statuses'
 import { MAX_WORD_LENGTH, REVEAL_TIME_MS } from '../../constants/settings'
+import { getStoredIsHighContrastMode, getStoredIsLoveMode } from '../../lib/localStorage'
 
 type Props = {
   children?: ReactNode
@@ -21,6 +22,8 @@ export const Key = ({
   isRevealing,
 }: Props) => {
   const keyDelayMs = REVEAL_TIME_MS * MAX_WORD_LENGTH
+  const isHighContrast = getStoredIsHighContrastMode()
+  const isLove = getStoredIsLoveMode()
 
   const classes = classnames(
     'flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none dark:text-white',
@@ -29,10 +32,21 @@ export const Key = ({
       'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
         !status,
       'bg-slate-400 dark:bg-slate-800 text-white': status === 'absent',
+      
+      'bg-orange-500 hover:bg-orange-600 active:bg-orange-700 text-white':
+        status === 'correct' && isHighContrast && !isLove,
+      'bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 text-white':
+        status === 'present' && isHighContrast && !isLove,
+      
       'bg-green-500 hover:bg-green-600 active:bg-green-700 text-white':
-        status === 'correct',
-      'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white':
-        status === 'present',
+        status === 'correct' && !isHighContrast && !isLove,
+      'bg-yellow-500 hover:bg-yellow-600 active:bg-blue-700 text-white':
+        status === 'present' && !isHighContrast && !isLove,
+      
+      'bg-green-500 hover:bg-green-600 active:bg-red-700 text-white':
+        status === 'correct' && !isHighContrast && isLove,
+      'bg-yellow-500 hover:bg-yellow-600 active:bg-pink-700 text-white':
+        status === 'present' && !isHighContrast && isLove,
     }
   )
 
