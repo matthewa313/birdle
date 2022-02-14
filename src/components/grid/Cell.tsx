684d416d15d3e735f62a4672ce803808a9ceae0d
Matthew Anderson <mwanderson04@gmail.com>
2022-02-13 21:36:52 -0700

    Update Cell.tsx

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -1,6 +1,7 @@
 import { CharStatus } from '../../lib/statuses'
 import classnames from 'classnames'
 import { REVEAL_TIME_MS } from '../../constants/settings'
+import { getStoredIsHighContrastMode, getStoredIsLoveMode } from '../../lib/localStorage'
 
 type Props = {
   value?: string
@@ -20,6 +21,8 @@ export const Cell = ({
   const isFilled = value && !isCompleted
   const shouldReveal = isRevealing && isCompleted
   const animationDelay = `${position * REVEAL_TIME_MS}ms`
+  const isHighContrast = getStoredIsHighContrastMode()
+  const isLoveMode = getStoredIsLoveMode()
 
   const classes = classnames(
     'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
@@ -29,10 +32,21 @@ export const Cell = ({
       'border-black dark:border-slate-100': value && !status,
       'absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
         status === 'absent',
+       // high contrast mode
+      'correct shadowed bg-orange-500 text-white border-orange-500':
+        status === 'correct' && isHighContrast && !isLoveMode,
+      'present shadowed bg-cyan-500 text-white border-cyan-500':
+        status === 'present' && isHighContrast && !isLoveMode,
+       // normal mode
       'correct shadowed bg-green-500 text-white border-green-500':
-        status === 'correct',
-      'present shadowed bg-blue-500 text-white border-blue-500':
-        status === 'present',
+        status === 'correct' && !isHighContrast && !isLoveMode,
+      'present shadowed bg-yellow-500 text-white border-blue-500':
+        status === 'present' && !isHighContrast && !isLoveMode,
+      // love mode
+      'correct shadowed bg-green-500 text-white border-green-500':
+        status === 'correct' && !isHighContrast && isLoveMode,
+      'present shadowed bg-yellow-500 text-white border-blue-500':
+        status === 'present' && !isHighContrast && isLoveMode,
       'cell-fill-animation': isFilled,
       'cell-reveal': shouldReveal,
     }
