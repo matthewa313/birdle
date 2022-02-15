5c48224bb22bbbcb60ac4cb1e92dca5366df5243
Matthew Anderson <mwanderson04@gmail.com>
2022-02-15 11:19:47 -0700

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,7 +6,10 @@ export const WORDS = [
   'doves', // Mon Feb 14
   'hawks', // Tue Feb 15
   'finch', // Wed Feb 16
-  'quail', // Thu Feb 17
-  'booby', // Fri Feb 18
+  'booby', // Thu Feb 17
+  'quail', // Fri Feb 18
   'loons',
+  'raven',
+  'eagle',
+  'wrens',
 ]
